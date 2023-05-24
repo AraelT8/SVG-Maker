ad7adc09ccdecf050f2b85d6d6357acc120233f0
Arael <araeltiejerina@gmial.com>
2023-05-24 14:18:02 -0500

    added different shape

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ class SVG{
     render(){
         return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
     }
+    setShapeColor(color){
+        this.shapeColor = color;
+    }
     setText(text,color){
         this.textElement = `<text x="50%" y="50%" fill="${color}" font-size="50" dominant-baseline="middle" text-anchor="middle">${text}</text>`;
     }
@@ -53,6 +56,7 @@ function writeSVG(fileName, data){
 async function main(){
     const answers = await inquirer.prompt(prompts);
     const svg = new SVG();
+    svg.setShapeColor(answers['shape-color']);
     svg.setText(answers.text, answers['text-color']);
     switch(answers.shape){
         case 'Circle':
